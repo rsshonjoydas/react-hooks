d3b47bd51cc5169cdee2b5902b613e68bb2876ad
rsshonjoydas <rsshonjoydas@gmail.com>
2021-11-11 23:35:39 +0600

    useReducer complex state & action

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
 
 function App() {
   return (
     <div className="App">
       <h1>RS Shonjoy</h1>
       <CounterOne />
+      <br />
+      <CounterTwo />
     </div>
   );
 }
