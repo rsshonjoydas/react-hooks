4e83a24284a20c073c93600852128975999707c7
rsshonjoydas <rsshonjoydas@gmail.com>
2021-11-11 18:47:32 +0600

    Mouse move class component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import ClassCounterOne from './components/ClassCounterOne';
+import ClassMouse from './components/ClassMouse';
 import HookCounterOne from './components/HookCounterOne';
 
 function App() {
@@ -6,7 +7,8 @@ function App() {
     <div className="App">
       <h1>RS Shonjoy</h1>
       <ClassCounterOne /> <br />
-      <HookCounterOne />
+      <HookCounterOne /> <br />
+      <ClassMouse />
     </div>
   );
 }
